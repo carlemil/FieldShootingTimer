c775843ce56f4bbc90b1b44f8a0675dc941eab57
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-10-09 20:00:52 +0200

    Added play button and animation of dial.

--- app/src/main/java/se/kjellstrand/fieldshootingtimer/MainActivity.kt
+++ app/src/main/java/se/kjellstrand/fieldshootingtimer/MainActivity.kt
@@ -5,15 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.isActive
 import se.kjellstrand.fieldshootingtimer.ui.theme.FieldShootingTimerTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,36 +51,71 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Timer() {
     FieldShootingTimerTheme {
-        val timeInSecondsForEachSegment = listOf(6f, 25f, 40f, 90f, 10f, 80f)
+        val timeInSecondsForEachSegment = listOf(3f, 10f, 8f, 3f)
         val totalTime = timeInSecondsForEachSegment.sum()
-        val currentTime = totalTime / 3
 
-        Box(
-            contentAlignment = Alignment.Center,
+        var isRunning by remember { mutableStateOf(false) }
+        var currentTime by remember { mutableFloatStateOf(0f) }
+
+        LaunchedEffect(isRunning) {
+            if (isRunning) {
+                val startTimeMillis = withFrameMillis { it }
+                var lastFrameTimeMillis = startTimeMillis
+                while (currentTime < totalTime && isActive) {
+                    val frameTimeMillis = withFrameMillis { it }
+                    val deltaTime = (frameTimeMillis - lastFrameTimeMillis) / 1000f
+                    currentTime += deltaTime
+                    if (currentTime >= totalTime) {
+                        currentTime = totalTime
+                        isRunning = false
+                        break
+                    }
+                    lastFrameTimeMillis = frameTimeMillis
+                }
+            }
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
-            Timer(
-                currentTime = currentTime,
-                dialColors = DialColors(
-                    colors = listOf(
-                        MaterialTheme.colorScheme.primary,
-                        MaterialTheme.colorScheme.primary.copy(alpha = 0.7f),
-                        MaterialTheme.colorScheme.primary.copy(alpha = 0.4f),
-                        Color.Red,
-                        Color.Blue,
-                        Color.Green
-                    )
-                ),
-                gapAngleDegrees = 30f,
-                timesForSegments = timeInSecondsForEachSegment,
-                ringThickness = 30.dp,
-                borderColor = Color.Black,
-                borderWidth = 2.dp,
-                size = 300.dp,
-                badgeRadius = 15.dp,
-                handColor = Color.White,
-                handThickness = 4.dp
-            )
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.weight(10f)
+            ) {
+                Timer(
+                    currentTime = currentTime,
+                    dialColors = DialColors(
+                        colors = listOf(
+                            Color.LightGray,
+                            Color.Yellow,
+                            Color.Green,
+                            Color.Red
+                        )
+                    ),
+                    gapAngleDegrees = 30f,
+                    timesForSegments = timeInSecondsForEachSegment,
+                    ringThickness = 30.dp,
+                    borderColor = Color.Black,
+                    borderWidth = 2.dp,
+                    size = 300.dp,
+                    badgeRadius = 15.dp,
+                    handColor = Color.White,
+                    handThickness = 4.dp
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(onClick = { isRunning = !isRunning }) {
+                if (isRunning) {
+                    Icon(imageVector = Icons.Default.Call, contentDescription = "Pause")
+                } else {
+                    Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "Play")
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(3f))
         }
     }
 }
